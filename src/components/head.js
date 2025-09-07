2ec17a5e5e965c648e30211e9b41a060db44bc18
Ibrahim Jamil <ibrahimjamil090@gmail.com>
2025-09-07 06:15:39 -0700

    new

--- src/components/head.js
+++ src/components/head.js
@@ -69,15 +69,6 @@ const Head = ({ metadata }) => (
     <meta name="msapplication-TileColor" content={config.colors.navy} />
     <meta name="msapplication-TileImage" content={msIcon144x144} />
     <meta name="theme-color" content={config.colors.navy} />
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17537413754">
-</script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-17537413754');
-</script>
   </Helmet>
 );
 
