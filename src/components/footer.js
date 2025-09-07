ca9c73cc53d1244098a13909da542a2835ba1dd2
Ibrahim Jamil <ibrahimjamil090@gmail.com>
2025-09-07 06:12:14 -0700

    new

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,6 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
